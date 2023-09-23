c549e247d68fcfc83126e3cd2244425b7e4dd06d
Abdulhamid <mobolaji2309@gmail.com>
2023-09-23 08:45:28 +0100

    updating

--- components/home/Header.js
+++ components/home/Header.js
@@ -1,14 +1,83 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
-
+import { MaterialIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
 const Header = () => {
   return (
-    <View>
-      <Text>Header</Text>
+    <View style={styles.container}>
+      <TouchableOpacity>
+        <Image
+          style={styles.logo}
+          source={require("../../assets/header-logo.png")}
+        />
+      </TouchableOpacity>
+      <View style={styles.iconsContainer}>
+        <TouchableOpacity>
+          <MaterialIcons style={styles.icon} name="add-box" size={30} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Ionicons
+            style={styles.icon}
+            name="ios-heart-outline"
+            size={30}
+            color="white"
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.unreadBadge}>
+            <Text style={styles.unreadBadgeText}> 10 </Text>
+          </View>
+          <FontAwesome5
+            style={styles.icon}
+            name="facebook-messenger"
+            size={24}
+            color="black"
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 export default Header;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    marginHorizontal: 20,
+  },
+  logo: {
+    width: 100,
+    height: 50,
+    resizeMode: "contain",
+  },
+  iconsContainer: {
+    flexDirection: "row",
+  },
+  icon: {
+    width: 30,
+    height: 30,
+    marginLeft: 10,
+    color: "white",
+    // backgroundColor: "black",
+  },
+  unreadBadge: {
+    backgroundColor: "#FF3250",
+    position: "absolute",
+    left: 20,
+    bottom: 18,
+    width: 25,
+    height: 18,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: 100,
+  },
+  unreadBadgeText: {
+    color: "white",
+    fontWeight: "600",
+  },
+});
